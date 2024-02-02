88c127d73df632f883f0d0e506c11ec7f3b7122b
balesniy <balesniy@yandex.ru>
2024-02-02 14:14:42 +0100

    force load lazy images

--- engine_scripts/onReady.cjs
+++ engine_scripts/onReady.cjs
@@ -1,6 +1,6 @@
 module.exports = async (page, scenario, vp) => {
     console.log('SCENARIO > ' + scenario.label);
-  
+
     // add more ready handlers here...
     await page.waitForFunction(() => {
       return document.fonts.ready.then(() => {
@@ -8,9 +8,17 @@ module.exports = async (page, scenario, vp) => {
         return true;
       });
     });
-  
+
+    await page.evaluate((scenario) => {
+      /** force load lazy images */
+      const lazyImages = document.querySelectorAll('img[loading="lazy"]');
+      lazyImages.forEach((i) => {
+        i.removeAttribute('loading');
+      });
+    }, scenario);
+
     // await require('./clickAndHoverHelper')(page, scenario);
-  
+
     if (scenario.showSelectors) {
       await Promise.all(
         scenario.showSelectors.map(async (selector) => {
@@ -24,4 +32,3 @@ module.exports = async (page, scenario, vp) => {
       );
     }
   };
-  
